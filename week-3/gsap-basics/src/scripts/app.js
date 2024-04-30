92503dc4e96b0d3894ff7fd8dc30034d4d98f5f1
adriaan-glibert-development <info@adriaanglibert.be>
2024-04-30 14:56:40 +0200

    ✨ GSAP Tweens and staggering

--- week-3/gsap-basics/src/scripts/app.js
+++ week-3/gsap-basics/src/scripts/app.js
@@ -1,2 +1,33 @@
 import gsap from "gsap";
 
+const $boxes = document.querySelectorAll('.box');
+
+gsap.to([$boxes, 'span'], {
+    x: 200,
+    xPercent: 20,
+    scaleX: 2,
+    backgroundColor: 'blue',
+    duration: 2,
+    repeat: 5,
+    yoyo: true,
+    delay: 1,
+    repeatDelay: 1,
+    ease: "elastic.inOut",
+    yoyoEase: "ease.inOut",
+    stagger: {
+        amount: 2,
+        from: "center"
+    },
+    onRepeat: () => {
+        gsap.to([$boxes, 'span'], {
+            rotate: -180
+        })
+    },
+    onComplete: () => {
+        gsap.to('.box', {
+            rotate: 90,
+            duration: 1
+        })
+    }
+});
+
